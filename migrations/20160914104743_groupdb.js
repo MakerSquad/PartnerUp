cd75f8eeacc21306806eaf06e38471cf9156b05c
rwalter215 <r.walter215@gmail.com>
2016-09-14 11:14:58 -0500

    added knex/migrations and created tables on db.js (#12)

---
+++ migrations/20160914104743_groupdb.js
@@ -0,0 +1,31 @@
+exports.up = (knex, Promise) => Promise.all([
+  knex.schema.createTableIfNotExists('roles', table => {
+    table.increments('id');
+    table.string('role_name');
+  }),
+  knex.schema.createTableIfNotExists('users', table => {
+    table.increments('id');
+    table.string('name');
+  }),
+  knex.schema.createTableIfNotExists('groups', table => {
+    table.increments('id');
+    table.string('name');
+  }),
+  knex.schema.createTableIfNotExists('user_group', table => {
+    table.integer('user_id');
+    table.integer('group_id');
+    table.integer('role_id');
+  }),
+  knex.schema.createTableIfNotExists('pairs', table => {
+    table.increments('id');
+    table.integer('user1_id');
+    table.integer('user2_id');
+  }),
+]);
+exports.down = (knex, Promise) => Promise.all([
+  knex.schema.dropTableIfExists('users'),
+  knex.schema.dropTableIfExists('groups'),
+  knex.schema.dropTableIfExists('user_group'),
+  knex.schema.dropTableIfExists('pairs'),
+  knex.schema.dropTableIfExists('roles'),
+]);
--- server/db.js
+++ server/db.js
@@ -1,16 +1,37 @@
 var pg = require('pg');
-
-pg.defaults.ssl = true;
-var database = process.env.DATABASE_URL || 'postgres://zjfcgmhjqmyjxr:Xl1eC5t0hNY8_f9kk5fU6I11qX@ec2-174-129-4-75.compute-1.amazonaws.com:5432/dcjevoin17mflh';
-
-pg.connect(database, function(err, client) {
-  if (err) throw err;
-
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
+const config = require('../knexfile');
+const env = 'development';
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: 'ec2-54-163-245-32.compute-1.amazonaws.com',
+    user: 'hzhzhysgpursgg',
+    password: 'Mkih7oW9Ek6dGdTSmyuVgxw3kr',
+    database: 'dd9bnae9j8734d',
+    ssl: true
+  }
 });
+
+knex.migrate.latest([config]);
+
+// knex.initDB = () => Promise.all([
+//   knex('users').insert([
+//     { name: 'Joe' },
+//     { name: 'Frank' },
+//     { name: 'Rob' },
+//     { name: 'Ryan' },
+//     { name: 'Gilbert' },
+//   ]),
+//   knex('groups').insert([
+//     { url: 'OMflBAXJJKc', channel_id: 1 },
+//     { url: 'x76VEPXYaI0', channel_id: 1 },
+//     { url: 'evj6y2xZCnM', channel_id: 1 },
+//     { url: '5XpU5M0ZCKM', channel_id: 2 },
+//     { url: '-hfKtUT4ISs', channel_id: 2 },
+//     { url: 'JYYsAxC0Dic', channel_id: 2 },
+//     { url: 'rbFvzRsDBN4', channel_id: 3 },
+//     { url: '-C_jPcUkVrM', channel_id: 3 },
+//     { url: 'FHtvDA0W34I', channel_id: 3 },
+//   ]),
+//   knex('JoinTable').insert([]),
+// ]);
