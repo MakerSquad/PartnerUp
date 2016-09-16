a423edecbd9d675925daf9513207b9369d3540cd
Elliot Cheung <elliotccheung@gmail.com>
2016-09-16 15:39:00 -0500

    Pairing algorithm now avoids repeat pairs (#26)
    * Now tracks past pairs and can find clashes
    
    * CheckClashes is now it's own function, clashes are now red
    
    * Rewrote the random functionality; still need to check if we've done n generations
    
    * Randomize now exits if recursing over a certain number of times
    
    * Added a space

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -37,6 +37,12 @@ angular.module('PU.main', ['PU.factories'])
   $scope.creatingGroup = false;
   $scope.modalUserList = [];
 
+  $scope.pastPairs = {};
+  $scope.finalized = true;
+  $scope.clashes = [];
+  var timeoutCounter = 0;
+  var timeoutThreshold = 2500; //number of iterations to run before we assume we're in an infinite loop
+
   $scope.changeClass = function(cls){
     $scope.loading = true;
     console.log("Switching to: ", cls);
@@ -66,7 +72,9 @@ angular.module('PU.main', ['PU.factories'])
     return arr;
   }
 
-  $scope.randomize = function(groupSize){
+  //THIS VERSION OF RANDOMIZE ROLLS THE WHOLE GROUP RANDOMLY
+  $scope.trueRandomize = function(groupSize){
+    console.log("Rolling");
     $scope.groups = [];
     var stus = $scope.students.slice();
 
@@ -80,13 +88,122 @@ angular.module('PU.main', ['PU.factories'])
       shuffled.push(stus.splice(randInd, 1)[0]);
     }
 
+    var clashIndexes = [];
+    var tempGroups = [];
+    var strandedIndexes = [];
     for(var i = 0; i < shuffled.length; i += groupSize){
-      console.log("$scope groups in for loop: ", $scope.groups);
-      $scope.groups.push(shuffled.slice(i, i+groupSize))
+      var group = shuffled.slice(i, i+groupSize);
+      $scope.groups.push(group);
+    }
+    checkClashes();
+    $scope.partnerUp = true;
+    $scope.finalized = false;
+    return $scope.groups;
+  }
+
+  $scope.randomize = function(groupSize){
+    timeoutCounter += 1;
+
+    if(timeoutCounter > timeoutThreshold){
+      alert(`Uh oh! We were unable to generate a list without repeating pairs; this is likely because ` +
+      `most of the possible pairs, if not all of them, have already occurred. Here's a random list anyway. Sorry!`);
+      timeoutCounter = 0;
+      return $scope.trueRandomize(groupSize);
+    }
+    $scope.groups = [];
+    var stus = $scope.students.slice();
+
+    var shuffled = [];
+    for(var i = 0; i < stus.length % groupSize; i++){
+      stus.push({user:{name: "Code Monkey", uid: "-1"}});
     }
 
+    while(stus.length){
+      var randInd = Math.floor(Math.random() * stus.length);
+      shuffled.push(stus.splice(randInd, 1)[0]);
+    }
+
+    while(shuffled.length){
+      for(var j = 1; j < shuffled.length; j++){
+        var failed = true;
+        var first = shuffled[0];
+        var group = [first];
+        var noClashes = true;
+        for(var k = 0; k < group.length; k++){
+          if($scope.pastPairs[group[k].user.uid]){
+            if($scope.pastPairs[group[k].user.uid][shuffled[j].user.uid]){
+              noClashes = false;
+              break;
+            }
+          }
+        }
+        if(noClashes){
+          group.push(shuffled[j]);
+          shuffled.splice(j, 1);
+        }else{
+          continue;
+        }
+        if(group.length === groupSize){
+          $scope.groups.push(group);
+          shuffled.splice(0, 1);
+          failed = false;
+          break;
+        }
+      }
+      if(failed){
+        return $scope.randomize(groupSize);
+      }
+    }
     $scope.partnerUp = true;
+    $scope.finalized = false;
+    checkClashes();
+    timeoutCounter = 0;
     return $scope.groups;
+  }
+
+  var checkClashes = function(){
+    $scope.clashes = [];
+    for(var i = 0; i < $scope.groups.length; i++){
+      var group = $scope.groups[i];
+      for(var j = 0; j < group.length; j++){
+        var pushed = false;
+        for(var k = j; k < group.length; k++){
+          if($scope.pastPairs[group[j].user.uid]){            
+            if($scope.pastPairs[group[j].user.uid][group[k].user.uid]){
+              $scope.clashes.push(group);
+              pushed = true;
+              break;
+            }
+          }
+        }
+        if(pushed){
+          break;
+        }
+      }
+    }
+  }
+
+  /*
+  * Finalize takes the current pairings and records them into the "past pairs" object
+  */
+
+  $scope.finalize = function(){
+    for(var i = 0; i < $scope.groups.length; i++){
+      for(var j = 0; j < $scope.groups[i].length; j++){
+        for(var k = j+1; k < $scope.groups[i].length; k++){
+          if(!$scope.pastPairs[$scope.groups[i][j].user.uid]){
+            $scope.pastPairs[$scope.groups[i][j].user.uid] = {};
+          }
+          $scope.pastPairs[$scope.groups[i][j].user.uid][$scope.groups[i][k].user.uid] = true;
+          if(!$scope.pastPairs[$scope.groups[i][k].user.uid]){
+            $scope.pastPairs[$scope.groups[i][k].user.uid] = {};
+          }
+          $scope.pastPairs[$scope.groups[i][k].user.uid][$scope.groups[i][j].user.uid] = true; 
+        }
+      }
+    }
+    console.log("Past pairs: ", $scope.pastPairs);
+    $scope.finalized = true;
   }
 
   $scope.removeFromStudent = function(student){
@@ -121,6 +238,7 @@ angular.module('PU.main', ['PU.factories'])
       $scope.selectedForSwap = null;
       $scope.selectedForSwapIndex = null;
     }
+    checkClashes();
   }
 
   var searchForSelected = function(student){
