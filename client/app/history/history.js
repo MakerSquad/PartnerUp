2fdff780bad2890da17e5bbf46820b833f77d50c
Kathryn Hansen <katie112892@aol.com>
2016-09-19 12:04:03 -0500

    set up history pages (#40)

---
+++ client/app/history/history.js
@@ -0,0 +1,25 @@
+angular.module('PU.history', ['PU.factories'])
+
+.controller('HistoryController', function ($scope, $location, Makerpass, $http, $routeParams) {
+  //$routeParams.class //name id of the class! 
+  $scope.generationId = 1;
+  $scope.pastPairs = []
+  $scope.getHistory = function(classId, generationId){
+    //database function to get all data
+    console.log('database function called')
+  }
+  $scope.next = function(){
+
+    $scope.generationId++
+    $scope.getHistory($routeParams.class, $scope.generationId);
+  }
+  $scope.previous = function(){
+    if($scope.generationId>=2){
+    $scope.generationId--
+    }
+    $scope.getHistory($routeParams.class, $scope.generationId);
+  }
+
+
+
+})
