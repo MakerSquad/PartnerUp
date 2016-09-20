2fc49c7b29cfc9f3caefa6529a6443f71d2bda9d
rwalter215 <r.walter215@gmail.com>
2016-09-20 11:42:11 -0500

    Updated migrations to include generations table and ignore table (#51)
    * added findOrCreateAdmin
    
    * updated findOrCreateAdmin function
    
    * updated
    
    * updated migrations table to include ignore table, generation table, and gen_id
    
    * fixed migrations bug

--- migrations/20160914104743_groupdb.js
+++ migrations/20160914104743_groupdb.js
@@ -20,7 +20,18 @@ exports.up = (knex, Promise) => Promise.all([
     table.increments('id');
     table.integer('user1_id');
     table.integer('user2_id');
-    table.string('group_id')
+    table.integer('group_id');
+    table.integer('gen_id');
+  }),
+  knex.schema.createTableIfNotExists('generations', table => {
+    table.increments('id');
+    table.string('title');
+    table.integer('group_id');
+  }),
+  knex.schema.createTableIfNotExists('ignore', table => {
+    table.increments('id');
+    table.integer('user_id');
+    table.integer('group_id');
   })
 ])
 
@@ -28,5 +39,7 @@ exports.down = (knex, Promise) => Promise.all([
   knex.schema.dropTableIfExists('users'),
   knex.schema.dropTableIfExists('groups'),
   knex.schema.dropTableIfExists('user_group'),
-  knex.schema.dropTableIfExists('pairs')
+  knex.schema.dropTableIfExists('pairs'),
+  knex.schema.dropTableIfExists('ignore'),
+  knex.schema.dropTableIfExists('generations')
 ]);
