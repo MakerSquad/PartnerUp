5b4a175faf3a8d6fb2b25dd81584729d47577357
Kathryn Hansen <katie112892@aol.com>
2016-09-22 19:33:07 -0500

    Histohisto (#94)
    * history maintence
    
    * history had further improved

--- client/app/history/history.js
+++ client/app/history/history.js
@@ -2,69 +2,74 @@ angular.module('PU.history', ['PU.factories'])
 
 .controller('HistoryController', function ($scope, $location, Makerpass, $http, $routeParams, DB) {
   
-  // //set up for copy to clipboard function 
   new Clipboard('.clipyclip');
-  $scope.groupSize = 2
+  $scope.groupSize = 2;
   $scope.generationId = 0; 
   $scope.pastPairs = [];
+  $scope.displayPairs = [];
   $scope.generations = [];
-  $scope.prev='prev'
-  $scope.nex= 'next'
-  $scope.currGen = 0
-  $scope.maxGen = 0
-  $scope.currClass = $routeParams.class //CURRENT CLASS ID!
-  $scope.badPartners = []
-  $scope.index = 0
+  $scope.prev='prev';
+  $scope.nex= 'next';
+  $scope.currGen = 0;
+  $scope.maxGen = 0;
+  $scope.currClass = $routeParams.class; //CURRENT CLASS ID!
+  $scope.badPartners = [];
+  $scope.index = 0;
   $scope.library = {};
 
   //*********************************************************************************
   //this will get all the past pairs from the database by class id and generation id
   //*********************************************************************************
+
   $scope.getGen = function(cls){
   return   DB.getGenerations($scope.currClass)
     .then(function(data){
-      console.log(data)
       for(var i = 0; i<data.length; i++){
-        $scope.generations.push(data[i])
+        $scope.generations.push(data[i]);
       }
-      console.log('dddddddd', $scope.generations)
-      $scope.currGen = $scope.generations[0].title
-      $scope.maxGen = $scope.generations.length-1
-      $scope.generationId = $scope.generations[0].id
-      console.log('fdfdfd',$scope.generationId)
+      $scope.currGen = $scope.generations[0].title;
+      $scope.maxGen = $scope.generations.length-1;
+      $scope.generationId = $scope.generations[0].id;
       if($scope.generations.length > 1){
-      $scope.prev=$scope.generations[$scope.generations.length-1].title
-      $scope.nex= $scope.generations[1].title
-    }
-    
+        $scope.prev=$scope.generations[$scope.generations.length-1].title;
+        $scope.nex= $scope.generations[1].title;
+      }
+    });
+  };
 
-  })
-  }
+  //*********************************************************************************
+  //calls the database and sets pastpairs to the data we get back
+  //*********************************************************************************
 
-  $scope.getHistory = function(classId){
+  $scope.getHistory = function(){
     //database function to get all data
    return DB.getPairs($scope.currClass)
       .then(function(data){
-        console.log('hihihi: ',data)
-        $scope.pastPairs = data
-        $scope.pastPairs = $scope.pastPairs.filter(m=>m.gen_table_id===$scope.generationId)
-        console.log('lalala: ',$scope.pastPairs)
-        
-      })
+        $scope.pastPairs = data;
+      });
+  };
 
-  }
+  //*********************************************************************************
+  //filters the data with the current generationId
+  //*********************************************************************************
+
+  $scope.changeGen = function(){
+    $scope.displayPairs = $scope.pastPairs.filter(pp => pp.gen_table_id===$scope.generationId);
+  };
+
+  //*********************************************************************************
+  //makes a library with names and uids so names will show up on the page and not the 
+  //uids
+  //*********************************************************************************
    
    $scope.makeMap = function (){
     return DB.getMemberships($scope.currClass)
-    .then(function(data){console.log('flaflafla', data[0].user)
+    .then(function(data){
       for (var i = 0; i<data.length; i++){
-        console.log(data[i].user)
-        $scope.library[data[i].user.uid]= data[i].user.name
+        $scope.library[data[i].user.uid]= data[i].user.name;
       }
-  })
-
+    });
    }
-
 
   // *********************************************************************************
   // this sets the generation and sets the next and previous generations so that they
@@ -75,25 +80,22 @@ angular.module('PU.history', ['PU.factories'])
   $scope.setGen = function(generation){
     $scope.currGen = generation.title;
     $scope.groupSize = generation.group_size;
-    $scope.index = $scope.generations.indexOf(generation)
-    $scope.generationId = $scope.generations[$scope.index].id
+    $scope.index = $scope.generations.indexOf(generation);
+    $scope.generationId = $scope.generations[$scope.index].id;
     if($scope.index === 0){
-      $scope.nex = $scope.generations[$scope.index+1].title
-      $scope.prev = $scope.generations[$scope.generations.length-1].title
+      $scope.nex = $scope.generations[$scope.index+1].title;
+      $scope.prev = $scope.generations[$scope.generations.length-1].title;
     }
     else if($scope.index === $scope.maxGen){
-      $scope.prev = $scope.generations[$scope.index-1].title
-      $scope.nex = $scope.generations[0].title
+      $scope.prev = $scope.generations[$scope.index-1].title;
+      $scope.nex = $scope.generations[0].title;
     }
     else{
-      console.log("$scope.index: ", $scope.index);
-      $scope.prev = $scope.generations[$scope.index-1].title
-      $scope.nex = $scope.generations[$scope.index+1].title
+      $scope.prev = $scope.generations[$scope.index-1].title;
+      $scope.nex = $scope.generations[$scope.index+1].title;
     }
-
-    $scope.getHistory($scope.currClass, $scope.generations[$scope.index].id);
-
-  }
+    $scope.changeGen();
+  };
 
   //*********************************************************************************
   //When the next button is clicked, this sets the current generation to the next 
@@ -102,87 +104,101 @@ angular.module('PU.history', ['PU.factories'])
   //*********************************************************************************
 
   $scope.next = function(){
+    console.log("Bad partners, called in next: ", $scope.badPartners);
     if($scope.generations.length > 1){
-    if($scope.index >= $scope.maxGen){
-      $scope.index = 0;
-      $scope.prev=$scope.generations[($scope.generations.length-1)].title
-      $scope.currGen = $scope.generations[$scope.index].title
-      $scope.nex= $scope.generations[$scope.index+1].title
-      $scope.generationId = $scope.generations[$scope.index].id
-    }
-
-    else{
-      $scope.index++;
-      $scope.currGen = $scope.generations[($scope.index)].title
-      $scope.prev=$scope.generations[($scope.index-1)].title
-      $scope.generationId = $scope.generations[$scope.index].id
+      if($scope.index >= $scope.maxGen){
+        $scope.index = 0;
+        $scope.prev=$scope.generations[($scope.generations.length-1)].title;
+        $scope.currGen = $scope.generations[$scope.index].title;
+        $scope.nex= $scope.generations[$scope.index+1].title;
+        $scope.generationId = $scope.generations[$scope.index].id;
+      }
+      else{
+        $scope.index++;
+        $scope.currGen = $scope.generations[($scope.index)].title;
+        $scope.prev=$scope.generations[($scope.index-1)].title;
+        $scope.generationId = $scope.generations[$scope.index].id;
       if($scope.index === $scope.maxGen){
         $scope.nex = $scope.generations[0].title;
       }
       else{
-        $scope.nex= $scope.generations[$scope.index+1].title
+        $scope.nex= $scope.generations[$scope.index+1].title;
       }
     }
-
-    $scope.getHistory($routeParams.class, $scope.generations[$scope.index].id);
+    $scope.changeGen();
   }
-}
+};
+
   //*********************************************************************************
   //When the preious button is clicked, this sets the current generation to the previous 
   //generation and sets the prev and next generations and then calls get history to 
   //get the history of the current generation
   //*********************************************************************************
+
   $scope.previous = function(){
     if($scope.generations.length > 1){
-    if($scope.index>=1){
-    $scope.index--
-    $scope.currGen = $scope.generations[($scope.index)].title
-    $scope.nex= $scope.generations[$scope.index+1].title
-    $scope.generationId = $scope.generations[$scope.index].id
-      if($scope.index=== 0){
-        $scope.prev = $scope.generations[($scope.maxGen)].title
+      if($scope.index>=1){
+        $scope.index--;
+        $scope.currGen = $scope.generations[($scope.index)].title;
+        $scope.nex= $scope.generations[$scope.index+1].title;
+        $scope.generationId = $scope.generations[$scope.index].id;
+        if($scope.index=== 0){
+          $scope.prev = $scope.generations[($scope.maxGen)].title;
+        }
+        else{
+          $scope.prev=$scope.generations[($scope.index-1)].title;
+        }
       }
-      else{
-        $scope.prev=$scope.generations[($scope.index-1)].title
+      else if($scope.index <= 0){
+        $scope.index = $scope.maxGen;
+        $scope.currGen = $scope.generations[($scope.index)].title;
+        $scope.nex= $scope.generations[0].title;
+        $scope.prev=$scope.generations[($scope.generations.length-2)].title;
+        $scope.generationId = $scope.generations[$scope.index].id;
       }
-    }
-    else if($scope.index <= 0){
-      $scope.index = $scope.maxGen
-      $scope.currGen = $scope.generations[($scope.index)].title
-      $scope.nex= $scope.generations[0].title
-      $scope.prev=$scope.generations[($scope.generations.length-2)].title
-      $scope.generationId = $scope.generations[$scope.index].id
-    }
-    $scope.getHistory($routeParams.class, $scope.generations[$scope.index].id);
+    $scope.changeGen();
   }
-}
+};
+
+  //*********************************************************************************
+  //allows you set bad partners and remove bad partners
+  //*********************************************************************************
 
   $scope.toggleBadPartners = function(pair){
-    console.log(pair)
-      $scope.badPartners.push(pair);
-    
-      console.log($scope.badPartners)
+    if($scope.badPartners.includes(pair)){
+      var index = $scope.badPartners.indexOf(pair)
+      var something = $scope.badPartners.splice(index,1)
     }
-
+    else{
+      $scope.badPartners.push(pair)
+    }
+  };
   
+  //*********************************************************************************
+  //for the table so we can have nice titles 
+  //*********************************************************************************
 
-    $scope.getIndexArray = function(num){
+  $scope.getIndexArray = function(num){
     var arr = [];
     for(var i = 0; i < num; i++){
       arr[i] = i;
     }
     return arr;
-  }
+  };
+
   //*********************************************************************************
   //takes you back to the homepage when homepage button is clicked 
   //*********************************************************************************
 
   $scope.goHome = function(){
     $location.path('/');
-  }
+  };
+
+  //*********************************************************************************
+  //sets the page up for success! calls all necessary functions. 
+  //*********************************************************************************
 
   var init = (function(){ //function that runs on load; it'll call all the fns to set up the page
-    
     
     $http({ //Check the current user; redirect if we aren't logged in
       method: "GET",
@@ -192,16 +208,15 @@ angular.module('PU.history', ['PU.factories'])
       if(resp.data === ""){
         $location.path('/signin');
       } 
-        $scope.getGen()
-        .then(function(stuff){$scope.getHistory()
-          .then(function(morestuff){$scope.makeMap()
-            .then(function(evenmorestuff){console.log('init complete')})
+      $scope.getGen()
+      .then(function(stuff){$scope.getHistory()
+        .then(function(morestuff){$scope.makeMap()
+          .then(function(evenmorestuff){
+            $scope.changeGen();
+            console.log('init complete')
           })
         })
-        
-      
+      })
     })
   }())
-
-  
-})
+});
