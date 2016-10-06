1154ba65e07bbaa5fccdf32d82b1baa0795532a6
Gilbert <gilbertbgarza@gmail.com>
2016-10-06 14:00:21 -0500

    Prepare for heroku

--- knexfile.js
+++ knexfile.js
@@ -10,25 +10,20 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   },
-  production: {
-    client: 'pg',
-    connection: {
-      host: 'ec2-54-163-245-32.compute-1.amazonaws.com',
-      user: 'hzhzhysgpursgg',
-      password: 'Mkih7oW9Ek6dGdTSmyuVgxw3kr',
-      database: 'dd9bnae9j8734d',
-      ssl: true,
-      port: 5432
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+
   test: {
     client: 'pg',
     connection: {
       database: 'partner_up_test'
     },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
       tableName: 'knex_migrations'
     }
