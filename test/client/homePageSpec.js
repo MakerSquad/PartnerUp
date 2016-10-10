5183a972fccad96fbd5e73fbef235351bdca77cf
Elliot Cheung <elliotccheung@gmail.com>
2016-10-10 09:29:17 -0500

    Cleanup (#244)
    * Deleted mainSpec
    
    * Linted homePageSpec

--- test/client/homePageSpec.js
+++ test/client/homePageSpec.js
@@ -1,7 +1,6 @@
-'use strict'
+'use strict';
 
 describe('The Home Page', function() {
-
   beforeEach(module('PU'));
   var $rootScope;
   var $httpBackend;
@@ -15,34 +14,33 @@ describe('The Home Page', function() {
   var deferred;
 
   var testPools = [
-        {
-        id: 1,
-        name:"abc",
-        role: "student",
-        size:2
-        },
-        {
-         id: 2,
-        name:"234",
-        role: "memberAdmin",
-        size:4
-        },
-        {
-         id: 3,
-        name:"def",
-        role: "fellow",
-        size:3
-        },
-        {
-        id: 4,
-        name:"!@#$",
-        role: "instructor",
-        size:5
-        },
-      ];
+    {
+      id: 1,
+      name: "abc",
+      role: "student",
+      size: 2
+    },
+    {
+      id: 2,
+      name: "234",
+      role: "memberAdmin",
+      size: 4
+    },
+    {
+      id: 3,
+      name: "def",
+      role: "fellow",
+      size: 3
+    },
+    {
+      id: 4,
+      name: "!@#$",
+      role: "instructor",
+      size: 5
+    }
+  ];
 
   beforeEach(inject(function($injector) {
-
     // mock out our dependencies
     $rootScope = $injector.get('$rootScope');
     $httpBackend = $injector.get('$httpBackend');
@@ -55,196 +53,93 @@ describe('The Home Page', function() {
     deferred = $q.defer();
     // spyOn($location, 'path').and.returnValue('/');
     var $controller = $injector.get('$controller');
-    
 
-    createController = function () {
+    createController = function() {
       return $controller('HomeController', {
         $scope: $scope,
-        $location: $location,
+        $location: $location
         // getClasses: getClasses
       });
     };
   }));
 
-   it('should have a $scope.pools', function(){
+  it('should have a $scope.pools', function() {
     createController();
     expect($scope.pools).toBeDefined();
-  })
-   it('showPools should be a function', function(){
+  });
+  it('showPools should be a function', function() {
     createController();
     expect($scope.showPools).toBeDefined();
-   })
+  });
 
-   it('should return a promise', function () {
+  it('should return a promise', function() {
     createController();
-  expect($scope.showPools().then).toBeDefined();
-});
-  it('goToCreatePool', function () {
-  createController();
-  expect($scope.goToCreatePool).toBeDefined();
-        //   spyOn($location, 'path');    
-        // expect($location.path).toHaveBeenCalledWith('/createPool');
-  // expect($scope.goToCreatePool()).toBe($location.path('/createPool'));
-});
-  it('should change location when clicking createPool button', inject(function() {   
-    createController(); 
-        spyOn($location, 'path');    
-        $scope.goToCreatePool();
-        expect($location.path).toHaveBeenCalledWith('/createPool');
-    }));
-    it('should change location when clicking a pools title', inject(function() {   
-    createController(); 
-        spyOn($location, 'path');    
-        var pool = { id: 1}
-        $scope.goToPool(pool);
-        expect($location.path).toHaveBeenCalledWith('/pools/1');
-    }));
+    expect($scope.showPools().then).toBeDefined();
+  });
 
-it('goToPool', function () {
+  it('goToCreatePool', function() {
     createController();
-  expect($scope.goToPool).toBeDefined();
-});
+    expect($scope.goToCreatePool).toBeDefined();
+    //   spyOn($location, 'path');
+    // expect($location.path).toHaveBeenCalledWith('/createPool');
+    // expect($scope.goToCreatePool()).toBe($location.path('/createPool'));
+  });
+  it('should change location when clicking createPool button', inject(function() {
+    createController();
+    spyOn($location, 'path');
+    $scope.goToCreatePool();
+    expect($location.path).toHaveBeenCalledWith('/createPool');
+  }));
+  it('should change location when clicking a pools title', inject(function() {
+    createController();
+    spyOn($location, 'path');
+    var pool = {id: 1};
+    $scope.goToPool(pool);
+    expect($location.path).toHaveBeenCalledWith('/pools/1');
+  }));
 
-it('showsPools should resolve a promise', function(done){
+  it('goToPool', function() {
+    createController();
+    expect($scope.goToPool).toBeDefined();
+  });
+
+  it('showsPools should resolve a promise', function(done) {
   // spyOn(DB, 'getClasses').and.returnValue(deferred.promise);
-  var mockPools = [{
+    var mockPools = [
+      {
         id: 1,
-        name:"abc",
+        name: "abc",
         role: "student",
-        size:2
-        },
-        {
-         id: 2,
-        name:"234",
+        size: 2
+      },
+      {
+        id: 2,
+        name: "234",
         role: "memberAdmin",
-        size:4
-        },
-        {
-         id: 3,
-        name:"def",
+        size: 4
+      },
+      {
+        id: 3,
+        name: "def",
         role: "fellow",
-        size:3
-        },
-        {
+        size: 3
+      },
+      {
         id: 4,
-        name:"!@#$",
+        name: "!@#$",
         role: "instructor",
-        size:5
-        }];
-        var currUser = {name: "test"};
-            $httpBackend.expectGET("home/home.html").respond([]);
+        size: 5
+      }];
+    var currUser = {name: "test"};
+    $httpBackend.expectGET("home/home.html").respond([]);
     $httpBackend.expectGET("/currentUser").respond(currUser);
     // $httpBackend.expectDELETE( `/group/${groupId}`);
     $httpBackend.expectGET("/groups").respond(mockPools);
   // deferred.resolve(mockPools);
-  createController();
-  $scope.$apply();
-  expect($scope.pools).not.toBe(undefined);
-  // expect($scope.pools).toEqual(mockPools);
-  done();
-})
-
-//   it('should resolve with mockPools', function () {
-//      var mockPools = [{
-//         id: 1,
-//         name:"abc",
-//         role: "student",
-//         size:2
-//         },
-//         {
-//          id: 2,
-//         name:"234",
-//         role: "memberAdmin",
-//         size:4
-//         },
-//         {
-//          id: 3,
-//         name:"def",
-//         role: "fellow",
-//         size:3
-//         },
-//         {
-//         id: 4,
-//         name:"!@#$",
-//         role: "instructor",
-//         size:5
-//         }];
-//     var currUser = {name: "test"};
-//     // $httpBackend.expectGET("home/home.html").respond([]);
-//     // $httpBackend.expectGET("/currentUser").respond(currUser);
-//     // $httpBackend.expectDELETE( `/group/${groupId}`);
-//     // $httpBackend.expectGET("/groups").respond(mockPools);
-//     createController();
-//     // $httpBackend.flush();
-
-//   var data;
-
-//   // set up a deferred
-//   var deferred = $q.defer();
-//   // get promise reference
-//   var promise = deferred.promise;
-
-//   // set up promise resolve callback
-//   promise.then(function (response) {
-//     data = response.success;
-//   });
-
-//   $scope.showPools().then(function(response) {
-//     // resolve our deferred with the response when it returns
-//     deferred.resolve(response);
-//   });
-
-//   // force `$digest` to resolve/reject deferreds
-//   $rootScope.$digest();
-
-//   // make your actual test
-//   expect(data).toEqual(mockPools);
-// });
-
-//   // it('should have the current class', function(){
-//   //   createController();
-//   //   $scope.pools = testPools;
-//   //   expect($scope.pools).toBe(testPools);
-//   // })
-//   //fake backend
-//   it('should shows pools in reverse order', function(){
-    
-   
-//     var mockPools = [{
-//         id: 1,
-//         name:"abc",
-//         role: "student",
-//         size:2
-//         },
-//         {
-//          id: 2,
-//         name:"234",
-//         role: "memberAdmin",
-//         size:4
-//         },
-//         {
-//          id: 3,
-//         name:"def",
-//         role: "fellow",
-//         size:3
-//         },
-//         {
-//         id: 4,
-//         name:"!@#$",
-//         role: "instructor",
-//         size:5
-//         }];
-//     var currUser = {name: "test"};
-//     $httpBackend.expectGET("home/home.html").respond([]);
-//     $httpBackend.expectGET("/currentUser").respond(currUser);
-//     $httpBackend.expectGET("/groups").respond(mockPools);
-//     // $httpBackend.expectDELETE( `/group/${groupId}`);
-//     createController();
-//     $httpBackend.flush();
-// expect($scope.showPools()).toEqual(mockPools);
-//     expect($scope.pools[0]).toEqual(mockPools[3]);
-//   })
-
-
-
-})
+    createController();
+    $scope.$apply();
+    expect($scope.pools).not.toBe(undefined);
+    // expect($scope.pools).toEqual(mockPools);
+    done();
+  });
+});
